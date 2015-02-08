f622b69f0ef01a3d249b52e7dc58244eb6ac16da
steve1994 <steve.harnadi@gmail.com>
2015-02-08 11:09:11 +0700

    Tutorial 1 Android

---
+++ app/src/main/java/com/example/steve/comvogellaandroidlifecycleactivity/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.steve.comvogellaandroidlifecycleactivity;
+
+
+import android.os.Bundle;
+
+
+public class SecondActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
